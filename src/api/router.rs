566dd2a3f2ff7b782cf390ea3a1b445c0c7fbde0
Tim Vosskühler <tim.vosskuehler@stud.h-da.de>
2025-01-05 21:38:30 +0100

    new endpoints

--- src/api/router.rs
+++ src/api/router.rs
@@ -1,7 +1,25 @@
 use axum::Router;
 use axum::routing::get;
 
+/**
+ * Initializing the api routes.
+ */
 pub async fn init_router() -> Router {
     Router::new() //add new routes here
-        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello-world", get(|| async { "Hello, World!" }))
+        .route("/notify", get(poll_for_new_messages))
+        .route("/timeline", get(scroll_chat_timeline))
+        .route("/send-msg", get(send_message))
+}
+
+async fn poll_for_new_messages() -> &'static str {
+    "Not Implemented"
+}
+
+async fn scroll_chat_timeline() -> &'static str {
+    "Not Implemented"
+}
+
+async fn send_message() -> &'static str {
+    "Not Implemented"
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use axum::Router;
 use log::{info};
 use ism::api::init_router;
+use tokio::net::TcpListener;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let env = env_logger::Env::default().default_filter_or("info");
     env_logger::Builder::from_env(env).init();
     info!("Starting ISM...");
-    let app = init_router().await;
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:7800").await.unwrap();
+    let app: Router = init_router().await;
+    let listener = TcpListener::bind("0.0.0.0:7800").await.unwrap();
+    info!("Server is listening on: 0.0.0.0:7800");
     axum::serve(listener, app).await.unwrap();
+    info!("Stopping ISM...");
 }

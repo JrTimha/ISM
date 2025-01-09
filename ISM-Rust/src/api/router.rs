bfed6f6d0232719256bd5d89ea5497e432ce26b1
Tim Vosskühler <tim.vosskuehler@stud.h-da.de>
2025-01-09 01:46:20 +0100

    Better Logs and Cors Config

--- ISM-Rust/src/api/router.rs
+++ ISM-Rust/src/api/router.rs
@@ -1,18 +1,30 @@
-use axum::http::StatusCode;
+use axum::http::{HeaderValue, Method, StatusCode};
 use axum::response::IntoResponse;
 use axum::{Json, Router};
+use axum::http::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 use axum::routing::get;
+use tower_http::cors::CorsLayer;
+use tower_http::trace::TraceLayer;
 use crate::database::get_db_instance;
 
 /**
  * Initializing the api routes.
  */
 pub async fn init_router() -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:4200".parse::<HeaderValue>().unwrap())
+        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
+        .allow_credentials(true)
+        .allow_methods([Method::GET, Method::POST]);
+
+
     Router::new() //add new routes here
         .route("/hello-world", get(|| async { "Hello, World!" }))
         .route("/notify", get(poll_for_new_messages))
         .route("/timeline", get(scroll_chat_timeline))
         .route("/send-msg", get(send_message))
+        .layer(TraceLayer::new_for_http())
+        .layer(cors)
 }
 
 async fn poll_for_new_messages() -> impl IntoResponse {
--- ISM-Rust/src/main.rs
+++ ISM-Rust/src/main.rs
@@ -5,12 +5,18 @@ use tokio::net::TcpListener;
 use ism::core::ISMConfig;
 use ism::api::init_router;
 use ism::database::{init_db};
+use tracing_subscriber::filter::LevelFilter;
 
+
+//learn it here: https://github.com/AarambhDevHub/rust-backend-axum
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
     let run_mode = env::var("ISM_MODE").unwrap_or_else(|_| "development".into());
     let config = ISMConfig::new_config(&run_mode).unwrap_or_else(|err| panic!("Missing needed env: {}", err));
-    init_logging(&config.log_level);
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
+
     info!("Starting ISM in {run_mode} mode.");
     init_db(&config).await;
     //init api router:
@@ -22,7 +28,7 @@ async fn main() {
     info!("Stopping ISM...");
 }
 
-fn init_logging(log_level: &str) {
-    let env = env_logger::Env::default().default_filter_or(log_level);
-    env_logger::Builder::from_env(env).init();
-}
+//fn init_logging(log_level: &str) {
+//    let env = env_logger::Env::default().default_filter_or(log_level);
+//    env_logger::Builder::from_env(env).init();
+//}

9344d10dd53c232a6ebdfd2ba1311a9b4dffd4ac
JrTimha <voss-tim@t-online.de>
2026-01-17 13:58:36 +0100

    Refactor `KeycloakAuthLayer` to allow unused variables and migrate `jsonwebtoken` from `aws_lc_rs` to `rust_crypto`, replacing and updating dependencies in `Cargo.{toml,lock}` accordingly.

--- src/keycloak/layer.rs
+++ src/keycloak/layer.rs
@@ -21,6 +21,7 @@ extern crate alloc;
 /// Authentication happens by looking for the `Authorization` header on requests and parsing the contained JWT bearer token.
 /// See the crate level documentation for how this layer can be created and used.
 #[derive(Clone, TypedBuilder)]
+#[allow(unused_variables)]
 pub struct KeycloakAuthLayer<R, Extra = ProfileAndEmail>
 where
     R: Role,

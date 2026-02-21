d58191a21e242eeb951c914c36645c1a08f3ea92
JrTimha <voss-tim@t-online.de>
2026-02-21 23:18:48 +0100

    add WebSocket support for server events, including `/api/wss` route and connection handling logic

--- src/messaging/routes.rs
+++ src/messaging/routes.rs
@@ -1,13 +1,14 @@
 use std::sync::Arc;
 use axum::Router;
-use axum::routing::{get, post};
+use axum::routing::{any, get, post};
 use crate::core::AppState;
 use crate::messaging::handler::handle_send_message;
-use crate::messaging::notifications::{get_latest_notification_events, stream_server_events};
+use crate::messaging::notifications::{get_latest_notification_events, stream_server_events, websocket_server_events};
 
 pub fn create_messaging_routes() -> Router<Arc<AppState>> {
     Router::new() //add new routes here
         .route("/api/notifications", get(get_latest_notification_events))
         .route("/api/sse", get(stream_server_events))
+        .route("/api/wss", any(websocket_server_events))
         .route("/api/send-msg", post(handle_send_message))
 }
